ad2784abebf8d50b5849539593d8c4a29ab4d44f
moko <ljun925@naver.com>
2024-04-04 02:27:55 +0900

    REST API 심화 HAL로 변경하기

--- routers/article.js
+++ routers/article.js
@@ -4,22 +4,58 @@ import db from '../db.js';
 
 const router = express.Router();
 
-router
-  .route('')
-  // 게시글 생성하기
-  .post((req, res) => {
-    const article = {
-      ...req.body,
-      id: randomUUID(),
-    };
-    db.data.articles.push(article);
-    db.write();
-    res.status(201).json(article);
-  })
-  // 게시글 조회
-  .get((req, res) => {
-    res.status(200).json(db.data.articles);
+// 게시글 생성하기
+router.post('', (req, res) => {
+  const article = {
+    ...req.body,
+    id: randomUUID(),
+  };
+
+  db.data.articles.push(article);
+  db.write();
+
+  res.set('Content-Type', 'application/vnd.hal+json');
+  res.status(201).json({
+    _links: {
+      self: {
+        href: req.originalUrl,
+      },
+    },
+    _embedded: {
+      article: {
+        _links: {
+          self: {
+            href: `${req.originalUrl}/${article.id}`,
+          },
+        },
+        ...article,
+      },
+    },
   });
+});
+
+// 게시글 조회
+router.get('', (req, res) => {
+  res.set('Content-Type', 'application/vnd.hal+json');
+
+  res.status(200).json({
+    _links: {
+      self: {
+        href: req.originalUrl,
+      },
+    },
+    _embedded: {
+      articles: db.data.articles.map((article) => ({
+        _links: {
+          self: {
+            href: `${req.originalUrl}/${article.id}`,
+          },
+        },
+        ...article,
+      })),
+    },
+  });
+});
 
 // 게시글 상세 조회하기
 router.get('/:articleId', (req, res) => {
@@ -27,10 +63,41 @@ router.get('/:articleId', (req, res) => {
   const article = db.data.articles.find(
     ({ id }) => id === req.params.articleId
   );
+
+  res.set('Content-Type', 'application/vnd.hal+json');
+
   if (!article) {
-    res.status(404).send();
+    res.status(404).json({
+      _links: {
+        self: {
+          href: req.originalUrl,
+        },
+        articles: {
+          href: req.baseUrl,
+        },
+      },
+      message: '조회하려는 게시글이 존재하지 않습니다.',
+      error: 'Not Found',
+    });
   }
-  res.status(200).json(article);
+
+  res.status(200).json({
+    _links: {
+      self: {
+        href: req.originalUrl,
+      },
+    },
+    _embedded: {
+      article: {
+        _links: {
+          self: {
+            href: `${req.originalUrl}`,
+          },
+        },
+        ...article,
+      },
+    },
+  });
 });
 
 // 게시글 수정하기 (모든 값을 호출 -> patch 처럼 값을 호출하면 원본 데이터 유실의 문제가 도래된다.)
@@ -38,13 +105,44 @@ router.put('/:articleId', (req, res) => {
   // article을 찾는 로직
   const { articleId } = req.params;
   const articleIndex = db.data.articles.findIndex(({ id }) => id === articleId);
+
+  res.set('Content-Type', 'application/vnd.hal+json');
   if (articleIndex < 0) {
-    res.status(404).send();
+    res.status(404).json({
+      _links: {
+        self: {
+          href: req.originalUrl,
+        },
+        articles: {
+          href: req.baseUrl,
+        },
+      },
+      message: '조회하려는 게시글이 존재하지 않습니다.',
+      error: 'Not Found',
+    });
   }
   const article = { ...req.body, id: articleId };
+
   db.data.articles[articleIndex] = article;
   db.write();
-  res.status(200).json(article);
+
+  res.status(200).json({
+    _links: {
+      self: {
+        href: req.originalUrl,
+      },
+    },
+    _embedded: {
+      article: {
+        _links: {
+          self: {
+            href: `${req.originalUrl}`,
+          },
+        },
+        ...article,
+      },
+    },
+  });
 });
 
 // 게시글 수정하기 (수정하려는 값만 호출)
@@ -52,16 +150,48 @@ router.patch('/:articleId', (req, res) => {
   // article을 찾는 로직
   const { articleId } = req.params;
   const articleIndex = db.data.articles.findIndex(({ id }) => id === articleId);
+
+  res.set('Content-Type', 'application/vnd.hal+json');
   if (articleIndex < 0) {
-    res.status(404).send();
+    res.status(404).json({
+      _links: {
+        self: {
+          href: req.originalUrl,
+        },
+        articles: {
+          href: req.baseUrl,
+        },
+      },
+      message: '조회하려는 게시글이 존재하지 않습니다.',
+      error: 'Not Found',
+    });
   }
+
   const article = db.data.articles[articleIndex];
   for (const key of Object.keys(req.body)) {
     article[key] = req.body[key];
   }
+
   db.data.articles[articleIndex] = article;
   db.write();
-  res.status(200).json(article);
+
+  res.status(200).json({
+    _links: {
+      self: {
+        href: req.originalUrl,
+      },
+    },
+    _embedded: {
+      article: {
+        _links: {
+          self: {
+            href: `${req.originalUrl}`,
+          },
+        },
+        ...article,
+      },
+    },
+  });
 });
 
 // 게시글 삭제하기
@@ -69,13 +199,38 @@ router.delete('/:articleId', (req, res) => {
   // article을 찾는 로직
   const { articleId } = req.params;
   const articleIndex = db.data.articles.findIndex(({ id }) => id === articleId);
+
+  res.set('Content-Type', 'application/vnd.hal+json');
   if (articleIndex < 0) {
-    res.status(404).send();
+    res.status(404).json({
+      _links: {
+        self: {
+          href: req.originalUrl,
+        },
+        articles: {
+          href: req.baseUrl,
+        },
+      },
+      message: '조회하려는 게시글이 존재하지 않습니다.',
+      error: 'Not Found',
+    });
   }
+
   db.data.articles.splice(articleIndex, 1);
   db.write();
+
   // 전달할게 없어서 204를 반환한다. 대체로 그렇다
-  res.status(204).send();
+  res.status(200).json({
+    _links: {
+      self: {
+        href: req.originalUrl,
+      },
+      articles: {
+        href: req.baseUrl,
+      },
+    },
+    message: '게시글을 성공적으로 삭제했습니다.',
+  });
 });
 
 export default router;
